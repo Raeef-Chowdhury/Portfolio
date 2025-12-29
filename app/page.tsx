158fa9559750859af5427fcb1088451697dac3a8
Raeef Chowdhury <raeef.chowdhury1@education.nsw.gov.au>
2025-12-29 15:31:07 +1100

    re optimize images for background & reoptimize portfolio card UI

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@ import Hero from "@/components/hero";
 import Projects from "@/components/projects";
 //TODO: BRIEF TOOLS LINE
 //TODO: BACKGROUND SHAPES/SVGS?????
-//TODO: Glass morphosim effect projects, left side screenshot with skewed/clipath then right side info.
+//TODO:Project Card Blog post detailed overview on click
 export default function Home() {
   return (
     <>
       <Header />
-      <Hero />
-      <Projects />
+      <main className="pb-[8rem]">
+        <Hero />
+        <Projects />
+      </main>
     </>
   );
 }
--- components/projects.tsx
+++ components/projects.tsx
@@ -13,20 +13,20 @@ function Projects() {
   return (
     <div
       id="projects"
-      className="max-w-[1440px] mx-auto mt-[24rem] flex flex-col items-center "
+      className="max-w-[1440px] mx-auto mt-[24rem]  flex flex-col items-center "
     >
       <Heading content="Projects" />
       <div className="Project__board mt-[8.4rem]">
-        <div className="border-primary hover:cursor-pointer hover:shadow-[0px_8px_40px_rgba(0,255,255,0.4)] hover:scale-[1.02] transition-all duration-300 border-4 max-w-[644px] rounded-2xl flex flex-col gap-[2.4rem]">
-          <div>
+        <div className="border-slate-700 overflow-hidden hover:cursor-pointer hover:shadow-[0px_8px_20px_rgba(255,255,255,0.1)] hover:scale-[1.02] transition-all duration-300 border-4 max-w-[840px] rounded-2xl flex flex-col gap-[2.4rem]">
+          <div className="max-h-[388px] max-w-[622px] rounded w-full bg-emerald-900/40 p-6">
             <Image
               src={StarVaultImg}
-              className="max-h-[424px]"
+              className="w-[110%]  object-cover rounded-xl"
               alt="Star Vault Project Image"
             />
           </div>
           <div className="text-[1.2rem] p-[1rem] ">
-            <h3 className="text-primary text-[3.6rem] underline  decoration-secondary underline-offset-2 justify-self-center w-[fit-content] text-center mb-[1.2rem]">
+            <h3 className="text-primary font-bold text-[4.8rem] underline  decoration-secondary underline-offset-2 justify-self-center w-[fit-content] text-center mb-[1.2rem]">
               Star Vault
             </h3>
             <p className="text-center text-text  max-w-[588px] leading-relaxed mb-[3rem] mx-auto text-[1.8rem] tracking-wide ">
@@ -34,7 +34,7 @@ function Projects() {
               to store their memories as stars inside a galaxy, where each
               galaxy represents an indiviudal category of personal expiriences.
             </p>
-            <ul className="flex gap-[1.2rem] justify-center">
+            <ul className="flex gap-[1.2rem] justify-center pb-[2.4rem]">
               {techStack.map((item, index) => (
                 <li
                   key={index}
